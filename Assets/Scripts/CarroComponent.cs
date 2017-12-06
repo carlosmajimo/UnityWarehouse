350d4c7b55de3f301ad75e96827c7fa7674157b7
Carlos Jimenez <carlosjimenez@MacBook-Pro-de-Carlos.local>
2017-12-06 09:16:42 -0500

    Commit

---
+++ Assets/Scripts/CarroComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarroComponent : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	float moveVertical;
+	float moveHorizontal;
+
+	void Start(){
+
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void Update (){
+		moveVertical = Input.GetAxis ("Vertical");
+		moveHorizontal = Input.GetAxis ("Horizontal");
+	}
+
+	void FixedUpdate (){
+
+		move ();
+		turn ();
+
+	}
+
+	void move(){
+
+		Vector3 movement = transform.forward * moveVertical * speed * Time.deltaTime;
+
+		rb.MovePosition (rb.position + movement);
+	}
+
+	void turn(){
+
+		float turn = moveHorizontal * speed * speed * Time.deltaTime;
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+		rb.MoveRotation (rb.rotation * turnRotation);
+	}
+}
